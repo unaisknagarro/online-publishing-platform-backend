b779f8415f3d97d8cb9c6f19436d6ab3e9bc1a2c
unaisknagarro <103625763+unaisknagarro@users.noreply.github.com>
2025-12-18 11:32:50 +0530

    Fixed some issues

--- src/config/db.js
+++ src/config/db.js
@@ -2,6 +2,8 @@
 const mongoose = require('mongoose');
 
 module.exports = async()=>{
-    await mongoose.connect(process.env.MONGO_URI, {});
+     console.log('preconnection.');
+    //await mongoose.connect(process.env.MONGO_URI, {});
+    await mongoose.connect('mongodb+srv://unaiskamle:unaisk1989@unaiscluster.r4n5di5.mongodb.net/OnlinePublishingPlatform?appName=UnaisCluster', {})
     console.log('MongoDB connected successfully');
 }
--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ const connectDB = require('./config/db');
 connectDB();
 
 
-app.listen(process.env.PORT, () => {
-console.log(`Server running on port ${process.env.PORT}`);
+// app.listen(process.env.PORT, () => {
+// console.log(`Server running on port ${process.env.PORT}`);
+// });
+
+app.listen(5000, () => {
+console.log(`Server running on port ${5000}`);
 });
